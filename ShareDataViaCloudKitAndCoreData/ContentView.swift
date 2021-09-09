b52fc0fbfb9b2a06d3bc980aa726f90806ae186a
Yang Xu <xuyang@me.com>
2021-09-09 15:07:51 +0800

    Initial Commit

---
+++ ShareDataViaCloudKitAndCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareDataViaCloudKitAndCoreData
+//
+//  Created by Yang Xu on 2021/9/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShareDataViaCloudKitAndCoreData/ShareDataViaCloudKitAndCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareDataViaCloudKitAndCoreDataApp.swift
+//  ShareDataViaCloudKitAndCoreData
+//
+//  Created by Yang Xu on 2021/9/9.
+//
+
+import SwiftUI
+
+@main
+struct ShareDataViaCloudKitAndCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
